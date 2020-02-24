92a2980304cdcaa3116042bee10b0b22e98ca63c
Neceros <neceros@gmail.com>
2020-02-24 13:10:44 -0800

    1.1 update

--- Source/BetterZzzt/HarmonyPatch_Zzzt.cs
+++ Source/BetterZzzt/HarmonyPatch_Zzzt.cs
@@ -1,42 +1,43 @@
-﻿using Harmony;
+﻿using HarmonyLib;
 using RimWorld;
 using System.Collections.Generic;
 using System.Linq;
-using OpCodes = System.Reflection.Emit.OpCodes;
-using Verse;
 using System.Reflection;
+using Verse;
+using OpCodes = System.Reflection.Emit.OpCodes;
 
 namespace BetterZzzt
 {
-  [HarmonyPatch(typeof(ShortCircuitUtility), "DrainBatteriesAndCauseExplosion")]
-  public static class BetterZzztHarmonyPatch
-  {
-    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    [HarmonyPatch(typeof(ShortCircuitUtility), "DrainBatteriesAndCauseExplosion")]
+    public static class BetterZzztHarmonyPatch
     {
-      List<CodeInstruction> instructionList = instructions.ToList();
-
-      for (int i = 0; i < instructionList.Count; i++)
-      {
-        CodeInstruction instruction = instructionList[i];
-
-        if(instruction.opcode == OpCodes.Callvirt && instruction.operand == AccessTools.Method(type: typeof(CompPowerBattery), name: nameof(CompPowerBattery.DrawPower)))
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-          yield return new CodeInstruction(opcode: OpCodes.Ldc_R4, operand: 1.33f);
-          yield return new CodeInstruction(opcode: OpCodes.Div);
+            List<CodeInstruction> instructionList = instructions.ToList();
+
+            for (int i = 0; i < instructionList.Count; i++)
+            {
+                CodeInstruction instruction = instructionList[i];
+
+                if (instruction.opcode == OpCodes.Callvirt &&
+                   instruction.OperandIs(AccessTools.Method(type: typeof(CompPowerBattery), name: nameof(CompPowerBattery.DrawPower))))
+                {
+                    yield return new CodeInstruction(opcode: OpCodes.Ldc_R4, operand: 1.33f);
+                    yield return new CodeInstruction(opcode: OpCodes.Div);
+                }
+
+                yield return instruction;
+            }
         }
-
-        yield return instruction;
-      }
     }
-  }
 
-  [StaticConstructorOnStartup]
-  public static class StartUp
-  {
-    static StartUp()
+    [StaticConstructorOnStartup]
+    public static class StartUp
     {
-      var harmony = HarmonyInstance.Create("BetterZzzt");
-      harmony.PatchAll(Assembly.GetExecutingAssembly());
+        static StartUp()
+        {
+            var harmony = new Harmony("BetterZzzt");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
     }
-  }
 }
